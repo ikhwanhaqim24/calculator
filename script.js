2213b3cdf02752480831c6e9a330cc821958b48a
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-20 15:37:44 +0800

    Initialize variables for each part of operation

--- script.js
+++ script.js
@@ -5,3 +5,7 @@ const subtract = (a, b) => { return a - b };
 const multiply = (a, b) => { return a * b };
 
 const divide = (a, b) => { return a / b };
+
+const firstNumber = 0;
+const secondNumber = 0;
+const operator = "add";
