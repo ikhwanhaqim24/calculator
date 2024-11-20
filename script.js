83217f1bcee5702e24fdda5a694a50eacdf6bc7d
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-20 18:57:48 +0800

    Add all button function calls on listener

--- script.js
+++ script.js
@@ -33,11 +33,79 @@ const buttons = document.querySelectorAll(".button");
 const displayCurrent = document.querySelector(".display > .current");
 const displayHistory = document.querySelector(".display > .history");
 
+let previousResult = 0;
+let currentResult = previousResult;
+
+const updateDisplay = (number = 0, history = 0) => {
+    // update current if any number button pressed
+    // update current with operands if any operands button pressed
+    // update current if delete button pressed
+
+};
+
+const resetDisplay = () => {
+    displayCurrent.textContent = "0";
+    displayHistory.textContent = "";
+}
+
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        if (button.id == "ac") {
-            displayCurrent.textContent = "0";
-            displayHistory.textContent = "0";
-        };
+        switch (button.id) {
+            // numbers
+            case "one":
+                displayCurrent.textContent += "1";
+            break;
+            case "two":
+                displayCurrent.textContent += "2";
+            break;
+            case "three":
+                displayCurrent.textContent += "3";
+            break;
+            case "four":
+                displayCurrent.textContent += "4";
+            break;
+            case "five":
+                displayCurrent.textContent += "5";
+            break;
+            case "six":
+                displayCurrent.textContent += "6";
+            break;
+            case "seven":
+                displayCurrent.textContent += "7";
+            break;
+            case "eight":
+                displayCurrent.textContent += "8";
+            break;
+            case "nine":
+                displayCurrent.textContent += "9";
+            break;
+            case "zero":
+                displayCurrent.textContent += "0";
+            break;
+            // special buttons
+            case "ac":
+                resetDisplay();
+            break;
+            case "delete":
+
+            break;
+            // operands
+            case "add":
+                
+            break;
+            case "subtract":
+                
+            break;
+            case "multiply":
+                
+            break;
+            case "divide":
+                
+            break;
+            // equals
+            case "equals":
+                
+            break;
+        }
     })
 })
