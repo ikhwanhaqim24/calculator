3257e1303a37b20bfd9eed66871de6af9f61caf6
Ikhwan Haqim <ikhwanhaqim24@gmail.com>
2024-11-20 15:49:02 +0800

    Add operate function

--- script.js
+++ script.js
@@ -6,6 +6,25 @@ const multiply = (a, b) => { return a * b };
 
 const divide = (a, b) => { return a / b };
 
-const firstNumber = 0;
-const secondNumber = 0;
-const operator = "add";
+let firstNumber = 0;
+let secondNumber = 0;
+let operator = "";
+
+const operate = (operator, firstNumber, secondNumber) => {
+    let result = 0;
+    switch (operator) {
+        case "add":
+            result = add(firstNumber, secondNumber);
+            break;
+        case "subtract":
+            result = subtract(firstNumber, secondNumber);
+            break;
+        case "multiply":
+            result = multiply(firstNumber, secondNumber);
+            break;
+        case "divide":
+            result = divide(firstNumber, secondNumber);
+            break;
+    };
+    return result;
+};
